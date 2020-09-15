4ae81b02083266bfb3936d2c38e35414e8084cb6
Nick Richardson <nickrichardson337@gmail.com>
2020-09-15 08:58:04 -0600

    added

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    items: [
+      {id:1, name:'Apple', complete:true},
+      {id:2, name:'Cereal', complete: false},
+      {id:3, name:'Milk', complete: false},
+      {id:4, name:'Soap', complete: false},
+    ],
+  };
+
+  renderItems = () => {
+    const { items } = this.state;
+    return items.map((item) => {
+      return <div key={item.id}>{item.name}</div>;
+    });
+  };
+
+  render() {
+    return(
+      <>
+      <h1>Grocery List</h1>
+      {this.renderItems()}
+      </>
+    );
+  }
 }
 
 export default App;
